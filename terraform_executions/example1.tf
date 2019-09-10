f069778e8824a2b9de0e0121831d24e139448a72
juligove <jagonzalezv@sura.com.co>
2019-09-10 14:36:45 -0500

    adding infra

--- terraform_executions/example1.tf
+++ terraform_executions/example1.tf
@@ -6,7 +6,25 @@ provider "aws" {
   region     = "sa-east-1"
 }
 
+# New resource for the S3 bucket our application will use.
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-getting-started-guide"
+  acl    = "private"
+}
+
+
 resource "aws_instance" "example1" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  # Tells Terraform that this EC2 instance must be created only after the
+  # S3 bucket has been created.
+  depends_on = [aws_s3_bucket.example]
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example1.id
 }
